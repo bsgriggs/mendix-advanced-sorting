acc8d68800dfc003f670c9a33b77d5f8f5d2350b
bsgriggs <benjaminsgriggs@gmail.com>
2022-08-10 17:15:29 -0400

    fixed on select

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -8,36 +8,27 @@ interface DropdownProps {
 }
 
 export function Dropdown(props: DropdownProps): ReactElement {
-    // const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    //     const dropdownValue = props.dropdownValues.find(
-    //         dropdownValue => dropdownValue.dropdownAttributeValue === event.target.value
-    //     );
-    //     console.log(dropdownValue);
-    //     if (dropdownValue !== undefined) {
-    //         props.selectOption(
-    //             dropdownValue.dropdownAttributeValue,
-    //             dropdownValue.setSortAscending,
-    //             dropdownValue.dropdownSortAscending === "true"
-    //         );
-    //     }
-    // };
-
     return (
         <select className="form-control" aria-haspopup="listbox" 
-        // onSelect={handleSelect}
+        onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
+            const dropdownValue = props.dropdownValues.find(
+                dropdownValue => dropdownValue.optionCaption.value === event.target.value
+            );
+            console.log("on change dropdown value",dropdownValue);
+            if (dropdownValue !== undefined) {
+                props.selectOption(
+                    dropdownValue.dropdownAttributeValue,
+                    dropdownValue.setSortAscending,
+                    dropdownValue.dropdownSortAscending === "true"
+                );
+            }
+        }}
         >
             {props.dropdownValues.map((dropdownValue, index) => (
                 <option
                     key={index}
-                    value={dropdownValue.dropdownAttributeValue}
+                    value={dropdownValue.optionCaption.value}
                     aria-selected={dropdownValue.dropdownAttributeValue === props.sortAttribute}
-                    onClick={()=> {
-                        props.selectOption(
-                            dropdownValue.dropdownAttributeValue,
-                            dropdownValue.setSortAscending,
-                            dropdownValue.dropdownSortAscending === "true"
-                        );
-                    }}
                 >
                     {dropdownValue.optionCaption.value}
                 </option>
